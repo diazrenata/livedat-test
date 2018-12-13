57cc80ae6c0963919ffa15435716c823695ef40e
Renata Diaz <diaz.renatam@gmail.com>
2018-12-13 12:46:48 -0500

    add testthat package

--- install-packages.R
+++ install-packages.R
@@ -1,6 +1,7 @@
 # Install pacman if it isn't already installed
 if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
 if ("dplyr" %in% rownames(installed.packages()) == FALSE) install.packages("dplyr")
+if ("testthat" %in% rownames(installed.packages()) == FALSE) install.packages("testthat")
 
 # Install packages required for analysis
 # Add any packages required for you data cleaning and manipulation to the
@@ -8,4 +9,4 @@ if ("dplyr" %in% rownames(installed.packages()) == FALSE) install.packages("dply
 # Do not remove the packages already listed here
 # they are important for running the livedat repository
 
-pacman::p_load(git2r, httr, semver, yaml, dplyr)
+pacman::p_load(git2r, httr, semver, yaml, dplyr, testthat)
